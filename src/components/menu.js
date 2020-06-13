270dde2fead90fc4bf393f0cfa20b147fdf8af61
shubham14p3 <shubham14p3@gmail.com>
2020-06-14 02:18:18 +0530

    Resume updated

--- src/components/menu.js
+++ src/components/menu.js
@@ -107,7 +107,7 @@ const Menu = ({ menuOpen, toggleMenu }) => {
                 </NavListItem>
               ))}
           </NavList>
-          <ResumeLink href="/resume.pdf" target="_blank" rel="nofollow noopener noreferrer">
+          <ResumeLink href="/Resume.pdf" target="_blank" rel="nofollow noopener noreferrer">
             Resume
           </ResumeLink>
         </NavLinks>
